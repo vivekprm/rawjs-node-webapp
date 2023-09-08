a246dfedeed9d2b6deafcf9a564805556c3b870f
Yuvraj Arora <yuviaroraxyz@gmail.com>
2023-09-08 10:53:19 +0530

    changed greeting

--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,7 @@ getHelloData();
 async function getHelloData() {
     const response = await fetch("http://localhost:3000/hello");
     const data = await response.json();
-    let msg = _.join(['Greetings: ', data.message], ' ');
+    let msg = _.join(['hello: ', data.message], ' ');
     let htmlContent = `
         <h1>${msg}</h1>
     `
